95186e1e45cf12c1089d04924f0c51f295762794
David Funk <davefunk135@gmail.com>
2023-01-25 20:57:33 -0700

    moving to the bedroom to finish this branch

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        try {
+            $issuedAt = time();
+            $expirationDate = time() * 3600; // 1hr
+
+            //code...
+        } catch (\Throwable $e) {
+            //throw $th;
+        }
+        $key = 'example_key';
+        $payload = array(
+            'iss' => 'http://localhost:3001',
+            'aud' => 'http://localhost:3001',
+            'iat' => 1356999524,
+            'nbf' => 1357000000
+        );
+
+
+
         return response()->json("done");
     }
 }
